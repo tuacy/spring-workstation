fae4acb89d953fe330b51ba0f41e3a29150b5254
tuacy <1007178106@qq.com>
2021-05-23 12:46:20 +0800

    完善sharding 5.0的使用

---
+++ common/src/main/java/com/tuacy/common/entity/web/controller/BaseController.java
@@ -0,0 +1,11 @@
+package com.tuacy.common.entity.web.controller;
+
+/**
+ * controller对应的基类，主要是应对一些方法的封装
+ *
+ * @author wuyx
+ * @version 1.0
+ * @date 2021/5/23 11:37
+ */
+public abstract class BaseController {
+}
